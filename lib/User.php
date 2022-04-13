7ab4f704be4d3c318a6c12d26e7ba87b6d2f8241
dev-sajid007 <dev.sajid007@gmail.com>
2022-04-13 21:01:13 +0600

    Usr Registration Complete

--- lib/User.php
+++ lib/User.php
@@ -9,4 +9,54 @@ class User{
     {
         $this->db = new Database();
     }
+
+
+    public function userRegistration($data){
+
+        $name     = $_POST['name'];
+        $username = $_POST['username'];
+        $email    = $_POST['email'];
+        $password = $_POST['password'];
+        $checkMail = $this->checkEmail($email);
+        if($name == '' || $username == '' || $email == '' || $password == ''){
+            return $msg = "<div class='alert alert-danger'>Error!</div>";
+        }
+
+        if($checkMail){
+            return $msg = "<div class='alert alert-danger'>Email Already Exist!</div>";
+        }
+
+        $sql = "INSERT INTO tbl_user (name,username,email,password) VALUES(:name,:username,:email,:password)";
+        $query = $this->db->pdo->prepare($sql);
+        $query->bindValue(':name',$name);
+        $query->bindValue(':username',$username);
+        $query->bindValue(':email',$email);
+        $query->bindValue(':password',$password);
+        $result = $query->execute();
+
+        if ($result) {
+             $msg = "<div class='alert alert-success'>Registration Successfully!</div>";
+             return $msg;
+        }
+        else{
+            $msg = "<div class='alert alert-danger'>Registration Problem!</div>";
+            return $msg;
+
+        }
+
+
+
+
+    }
+
+
+
+    public function checkEmail($email){
+        $sql = "SELECT email from tbl_user WHERE email = :email";
+        $query = $this->db->pdo->prepare($sql);
+        $query->bindValue(':email',$email);
+        $query->execute();
+
+        return $query->rowCount() > 0 ? true:false;
+    }
 }
--- register.php
+++ register.php
@@ -1,5 +1,14 @@
 <?php
-  include 'inc/Header.php'
+  include 'inc/Header.php';
+  include 'lib/User.php';
+?>
+
+<?php
+  $user = new User();
+  if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['register'])) {
+      $userReg = $user->userRegistration($_POST);
+      // print_r($_POST);
+  }
 ?>
 
     <div class="card mt-3">
@@ -8,25 +17,30 @@
       </div>
       <div class="card-body">
           <div class="div" style="max-width: 600px; margin:0 auto">
-            <form action="">
+            <?php
+              if (isset($userReg)) {
+                echo $userReg;
+              }
+            ?>
+            <form action="" method="POST">
                 <div class="form-group">
                     <label for="">Name</label>
-                    <input type="text" name="name" class="form-control" required>
+                    <input type="text" name="name" class="form-control">
                 </div>
                 <div class="form-group">
                     <label for="">User Name</label>
-                    <input type="text" name="name" class="form-control" required>
+                    <input type="text" name="username" class="form-control" >
                 </div>
                 <div class="form-group">
                     <label for="">Email</label>
-                    <input type="text" name="email" class="form-control" required>
+                    <input type="email" name="email" class="form-control" >
                 </div>
                 <div class="form-group">
                     <label for="">Password</label>
-                    <input type="password" name="email" class="form-control" required>
+                    <input type="password" name="password" class="form-control" >
                 </div>  
                 <div class="form-group">
-                    <button type="submit" class="btn btn-primary mt-2">Submit</button>
+                    <button type="submit" name="register" class="btn btn-primary mt-2">Submit</button>
                 </div>
             </form>
           </div>
